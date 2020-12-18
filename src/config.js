124a81363f41a45753bdd088a4c0ca88dbbc934a
Nate Voisan <rufio96@gmail.com>
2020-12-18 12:29:54 -0800

    final commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://aqueous-ravine-63814.herokuapp.com',
+  API_ENDPOINT: 'https://aqueous-ravine-63814.herokuapp.com/api',
   TOKEN_KEY: 'spaced-out-token',
 }
